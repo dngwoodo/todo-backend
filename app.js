a4dd7e6820e930151fa6f053c1f2d49550f70dbb
dngwoodo <dngwoodo@gmail.com>
2021-12-28 22:45:02 +0900

    create, read 구현

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+
+import cors from 'cors';
+
+import { v4 as uuidv4 } from 'uuid';
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors({ origin: '*'}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let todos = [];
+
+// 상태관리랑 같이하는걸로
+
+app.get('/todos', (req, res) => {
+  res.status(200).json(todos);
+}) // 읽기
+
+app.post('/todo', (req, res) => {
+  const { title } = req.body;
+  const newTodo = { id: uuidv4(), title, completed: false }
+
+  todos = [...todos, newTodo]
+
+  res.status(201).json({
+    status: 'ok',
+    message: '',
+    data: newTodo,
+  })
+}) // 추가
+
+app.delete('todo', (req, res) => {}) // 삭제
+
+app.patch('todo', () => {}) // 수정
+
+app.listen(PORT, () => {
+  console.log('내가 서버다.');
+})
